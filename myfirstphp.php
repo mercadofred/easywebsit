e4bfa31dc2a00bda6aa59ddc14d26e30cb7ec593
mercadofred <138345119+mercadofred@users.noreply.github.com>
2023-08-02 14:56:08 -0400

    adding title to php page

--- myfirstphp.php
+++ myfirstphp.php
@@ -4,15 +4,15 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title>Projects</title>
+    <title>PHP Projects</title>
   </head>
 
   <body>
 <h1>My first PHP page</h1>    
 
 <?php
-$var = "PHPPPPPPpP";
-echo "My first $var script!";
+$var = "PHP";
+echo "This is my first $var script!";
 ?>
 
 </body>
