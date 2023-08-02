90492842766605e07a0e23ec9d1ad07d24a5415d
mercadofred <138345119+mercadofred@users.noreply.github.com>
2023-08-02 15:41:59 -0400

    s

--- myfirstphp.php
+++ myfirstphp.php
@@ -10,7 +10,7 @@
   <body>
 <h1>My first PHP page</h1>    
 
-<p><?php use paragraph to separate text in php?></p>
+<p><?php echo "use <p>...</p> to separate text in php" ?></p>
 
 <p><?php echo "Hello PHP World!";?></p>
 
